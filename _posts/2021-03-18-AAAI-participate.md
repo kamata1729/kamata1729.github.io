cceb7ed1df111e469c0fe7581c8f77ce9ddeb9f5
Kamata <1216kamata@gmail.com>
2022-03-27 23:42:59 +0900

    commit0327

---
+++ _posts/2021-03-18-AAAI-participate.md
@@ -0,0 +1,19 @@
+---
+title: AAAI2022参加報告
+layout: post
+post-image: "/assets/images/logo_aaai.png"
+description: AAAI2022参加報告
+tags:
+- AAAI
+type: none
+---
+
+2/22-3/2でオンライン開催された、AAAI2022の参加記録です
+
+---
+
+# AAAI2022
+AAAIは人工知能系のトップカンファレンスの一つです。
+# Gather会場
+
+# 
---
+++ trigger-gh-pages-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script will trigger a rebuild on a repo's GitHub Pages site,
+# without having to create a new commit.
+# To use this script:
+# 1. Save this script to an easilyt accessible place, where you'll run it.
+# 2. Create a personal access token on GitHub. In Your profile > Settings
+#    > Developer settings > Personal access tokens
+# 3. Save the token in a text file. Idealy, alongside the script as token.txt
+# 4. Run the script from the Terminal with ./trigger-gh-pages-build.sh
+# 5. It will ask you for your GitHub username, the repo name, and the token file.
+#
+# Thanks to https://stackoverflow.com/a/54088985/1781075
+
+
+curl -u kamata1729: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 -X POST https://api.github.com/repos/kamata1729/kamata1729.github.io/pages/builds
